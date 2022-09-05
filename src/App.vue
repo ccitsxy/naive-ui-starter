25ce15e1fc9da8dc3560dfe5dad358a0d331d6d4
ccitsxy <1127885451@qq.com>
2022-09-05 11:44:47 +0800

    chore: update deps

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <script setup lang="ts">
 import { shallowRef, provide } from 'vue'
 
-import { zhCN } from 'naive-ui'
-import type { GlobalTheme } from 'naive-ui'
+import { zhCN, dateZhCN } from 'naive-ui'
+import type { GlobalTheme, GlobalThemeOverrides } from 'naive-ui'
 
 const theme = shallowRef<GlobalTheme | null | undefined>()
 provide('theme', theme)
+
+const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    fontFamily:
+      'Arial, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+  },
+}
 </script>
 
 <template>
   <n-config-provider
-    :locale="zhCN"
     abstract
     inline-theme-disabled
+    :locale="zhCN"
+    :date-locale="dateZhCN"
     :theme="theme"
+    :theme-overrides="themeOverrides"
   >
     <n-global-style />
     <router-view />
